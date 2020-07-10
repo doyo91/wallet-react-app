853e2b9f7225174aae68a5f209f5e75550bdb3fd
doyo91 <doyo_91@hotmail.com>
2020-07-10 12:39:48 +0200

    Updates

---
+++ precache-manifest.5c8c9de69119344e2ea924e9ca15dd75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea128043f68e372748eca6fcb0a1ae5",
+    "url": "/budget-react-app/index.html"
+  },
+  {
+    "revision": "ff11551d2e776f035a15",
+    "url": "/budget-react-app/static/css/main.08e117aa.chunk.css"
+  },
+  {
+    "revision": "082062b3a23a30afffc2",
+    "url": "/budget-react-app/static/js/2.4db3e869.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget-react-app/static/js/2.4db3e869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff11551d2e776f035a15",
+    "url": "/budget-react-app/static/js/main.85e6574b.chunk.js"
+  },
+  {
+    "revision": "a2b9169ae57e9fa8b378",
+    "url": "/budget-react-app/static/js/runtime-main.e2563236.js"
+  }
+]);
